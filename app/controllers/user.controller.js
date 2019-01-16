191333e3f136998ebecff559bd9114f8ddf98341
sagarSonawane <sonawanesagar3@gmail.com>
2019-01-16 19:31:19 +0530

    all crud operation

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,5 +1,6 @@
 const User=require('../model/user.model.js');
 
+// create user
 exports.create=(req,res)=>{
   if(!req.body.name)
   {
@@ -18,12 +19,15 @@ exports.create=(req,res)=>{
   })
 }
 
+// retrive all user
 exports.findAll=(req,res)=>
 {
 User.find().sort({userId:1}).then(user=>res.send(user)).catch(err=>{
   res.status(500).send({message:err.message || "Some error occurred while retrieving the user."});
 });
 }
+
+// retrive single user
 exports.finOne=(req,res)=>{
   var id=req.params.id;
 
@@ -36,5 +40,43 @@ User.findOne({userId:id}).then(
     res.send(user);
   }).catch(err=>{
   res.status(500).send({message:err.message || "Some error occurred while retrieving the user."});
+})
+}
+
+// delete single user
+exports.deleteOne=(req,res)=>{
+  if(!req.body.id)
+  {
+    res.status(404).send({'message':'Id can not be empty'})
+  }
+  User.deleteOne({userId:req.body.id}).then(user=>{
+    if(!user)
+    {
+      res.status(404).send({'message':'User not found with id '+id})
+    }
+    res.send('User deleted successfully');
+  }).catch(err=>{
+    res.status(500).send({'message':err.message || "Some error occurred while deleting user"})
+  })
+}
+
+// update record
+
+exports.updateRecord=(req,res)=>
+{
+  if(!req.body.id)
+  {
+    res.status(404).send({'message':'Id can not be empty'})
+  }
+  var myQuery={userId:req.body.id}
+  var newValues={$set:{name:req.body.name}};
+  User.updateOne(myQuery,newValues).then(user=>{
+    if(!user)
+    {
+      res.status(404).send({'message':'User not found with id '+id})
+    }
+    res.send(user);
+  }).catch(err=>{
+    res.status(500).send({'message':err.message || "Some error occurred while deleting user"})
 })
 }
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -4,5 +4,6 @@ const user=require('../controllers/user.controller.js')
   app.post('/newUser',user.create);
   app.get('/users',user.findAll);
   app.get('/user/:id',user.finOne)
-
+  app.post('/deleteUser',user.deleteOne)
+  app.post('/updateUser',user.updateRecord)
 }
